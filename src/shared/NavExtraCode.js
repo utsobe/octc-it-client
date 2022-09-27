59934f0fa197841d5f01bc4aa3c7dd3268e4fb21
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-09-27 16:57:37 +0800

    login and register button added to navber

---
+++ src/shared/NavExtraCode.js
@@ -0,0 +1,34 @@
+<li>
+    <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            {/* clickable image */}
+            <div class="w-9 rounded-full">
+                <img src="https://placeimg.com/80/80/people" alt='...' />
+            </div>
+        </label>
+        <div tabindex="2" class="dropdown-content mt-3 shadow-lg bg-base-100 rounded-box w-[280px]">
+            <div className='w-full p-2'>
+                {/* profile image */}
+                <div class="w-16 mx-auto">
+                    <img className='rounded-full' src="https://placeimg.com/80/80/people" alt='...' />
+                </div>
+                <div className='mt-3 text-center'>
+                    <p className='text-md font-semibold'>Anisul Islam Khan</p>
+                    <p className='text-[11px]'>Student ID: OCTCG-0075</p>
+                </div>
+                <div className='flex justify-evenly pt-2 pb-1'>
+                    <button className='text-sm py-2.5 px-5 bg-primary rounded-full text-white'>My Course</button>
+                    <button className='text-sm py-2.5 px-5 bg-primary rounded-full text-white'>View Profile</button>
+                </div>
+            </div>
+            <hr />
+            <div className='p-4 text-sm'>
+                <li className='mb-2 cursor-pointer'>
+                    {/* <a className='flex items-center'><FaRegMoon className='mr-1.5' />Darkmode</a> */}
+                </li>
+                {/* <li><a>Account</a></li> */}
+                {/* <li><a className='flex items-center'><FaSignOutAlt className='mr-1.5' />Logout</a></li> */}
+            </div>
+        </div>
+    </div>
+</li>
--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -10,41 +10,10 @@ const Navbar = ({ children }) => {
         <li><CustomLink className='mr-8 text-sm font-medium' to='/courses'>Courses</CustomLink></li>
         <li><CustomLink className='mr-8 text-sm font-medium' to='/about'>About</CustomLink></li>
         <li><CustomLink className='mr-8 text-sm font-medium' to='/contact'>Contact</CustomLink></li>
-        {/* image for modal */}
-        <li>
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    {/* clickable image */}
-                    <div class="w-9 rounded-full">
-                        <img src="https://placeimg.com/80/80/people" alt='...' />
-                    </div>
-                </label>
-                <div tabindex="2" class="dropdown-content mt-3 shadow-lg bg-base-100 rounded-box w-[280px]">
-                    <div className='w-full p-2'>
-                        {/* profile image */}
-                        <div class="w-16 mx-auto">
-                            <img className='rounded-full' src="https://placeimg.com/80/80/people" alt='...' />
-                        </div>
-                        <div className='mt-3 text-center'>
-                            <p className='text-md font-semibold'>Anisul Islam Khan</p>
-                            <p className='text-[11px]'>Student ID: OCTCG-0075</p>
-                        </div>
-                        <div className='flex justify-evenly pt-2 pb-1'>
-                            <button className='text-sm py-2.5 px-5 bg-primary rounded-full text-white'>My Course</button>
-                            <button className='text-sm py-2.5 px-5 bg-primary rounded-full text-white'>View Profile</button>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className='p-4 text-sm'>
-                        <li className='mb-2 cursor-pointer'>
-                            <a className='flex items-center'><FaRegMoon className='mr-1.5' />Darkmode</a>
-                        </li>
-                        {/* <li><a>Account</a></li> */}
-                        <li><a className='flex items-center'><FaSignOutAlt className='mr-1.5' />Logout</a></li>
-                    </div>
-                </div>
-            </div>
-        </li>
+        <li><Link className='mr-4 text-sm text-primary font-semibold border border-primary rounded-md px-5 py-3 hover:bg-primary hover:text-base-100 duration-200' to='/contact'>Login</Link></li>
+        <li><Link className='mr-8 text-sm text-base-100 font-medium bg-primary py-3 px-5 border border-primary rounded-md hover:bg-secondary hover:border-secondary duration-200' to='/contact'>Register</Link></li>
+        {/*==================== image for modal here from nav extra code ==============*/}
+
     </>
     return (
         <div class="drawer drawer-end">
