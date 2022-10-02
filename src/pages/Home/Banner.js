ff1b58ad909d57a4c2771dbeacfd1a41a8ffec4e
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-10-02 12:24:10 +0800

    banner updated for desktop view

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -21,17 +22,17 @@ import bnr4 from '../../assets/bnr-images/bnr-4.png';
 const bnrData = [
     {
         image: bnr1,
-        bnrTitle: 'This is the banner title',
+        bnrTitle: 'Microsoft Office Course',
         bnrBody: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, sit reprehenderit unde fuga nemo repellat commodi illo. Doloribus animi provident reprehenderit ducimus consequuntur recusandae doloremque incidunt, ipsa officiis eum quis.'
     },
     {
         image: bnr2,
-        bnrTitle: 'This is the banner title',
+        bnrTitle: 'Complete Graphic Design Course',
         bnrBody: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, sit reprehenderit unde fuga nemo repellat commodi illo. Doloribus animi provident reprehenderit ducimus consequuntur recusandae doloremque incidunt, ipsa officiis eum quis.'
     },
     {
         image: bnr3,
-        bnrTitle: 'This is the banner title',
+        bnrTitle: 'Digital Marketing Course',
         bnrBody: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, sit reprehenderit unde fuga nemo repellat commodi illo. Doloribus animi provident reprehenderit ducimus consequuntur recusandae doloremque incidunt, ipsa officiis eum quis.'
     },
 ];
@@ -41,7 +42,7 @@ console.log(bnrData.length);
 
 const Banner = () => {
     return (
-        <div className="lg:max-w-7xl mx-auto lg:px-12">
+        <div className="lg:max-w-7xl mx-auto lg:px-12 pt-3">
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -58,13 +59,14 @@ const Banner = () => {
 
                 {
                     bnrData.map(data =>
-                        <SwiperSlide className="min-h-screen lg:px-12">
+                        <SwiperSlide className="h-full lg:px-7 py-5">
                             <div className="flex items-center justify-between">
                                 <div className="w-full">
-                                    <h1 className="text-3xl font-bold">{data.bnrTitle}</h1>
-                                    <p className="font-light">{data.bnrBody}</p>
+                                    <h1 className="text-5xl font-semibold">{data.bnrTitle}</h1>
+                                    <p className="font-light py-5">{data.bnrBody}</p>
+                                    <button className="text-base-100 py-3 px-6 bg-primary rounded-md hover:bg-secondary  duration-200 flex items-center">Enroll Now <FaArrowRight className="ml-2" /></button>
                                 </div>
-                                <div className="w-full">
+                                <div className="w-full max-h-[500px] p-7">
                                     <img src={data.image} alt="" />
                                 </div>
                             </div>
