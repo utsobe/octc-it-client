2bd6a1b17e901128bb98369fffd6ac66476b7072
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-10-11 19:19:19 +0800

    popular course section updated

--- src/pages/Home/CoursesSection.js
+++ src/pages/Home/CoursesSection.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import course1 from '../../assets/course-images/graphic-img.jpg';
+import MsOffice from '../../assets/course-images/ms-img.jpg';
+import Graphic from '../../assets/course-images/graphic-img.jpg';
+import Digital from '../../assets/course-images/digital-img.jpg';
 
 
 const courses = [
     {
         title: 'Microsoft Office',
-        image: course1,
+        image: MsOffice,
         shortDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nesciunt provident',
         price: 7000
     },
     {
         title: 'Graphic Design',
-        image: course1,
+        image: Graphic,
         shortDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nesciunt provident',
         price: 7000
     },
     {
         title: 'Digital Marketing',
-        image: course1,
+        image: Digital,
         shortDescription: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nesciunt provident',
         price: 7000
     },
@@ -32,46 +34,24 @@ const CoursesSection = () => {
                 <p className='px-3 lg:px-72'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nesciunt provident magnam distinctio similique modi amet consectetur adipisicing elit. A nesciunt provident</p>
             </div>
             <div className='grid grid-cols-3 gap-10 mt-12'>
-                <div className='border rounded-md'>
-                    <div>
-                        <img src={course1} className='rounded-t-md' alt="" />
-                    </div>
-                    <div className='p-4'>
-                        <h2 className='text-2xl font-semibold'>Graphic Design</h2>
-                        <p className='font-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, numquam.</p>
-                        <p>Course fee: 7000$</p>
-                        <div className='mt-4 flex justify-end'>
-                            <button className='mr-4 text-sm text-primary font-semibold border border-primary rounded-md px-5 py-3 hover:bg-primary hover:text-base-100 duration-200'>Show More</button>
-                            <button className='text-sm text-base-100 font-medium bg-primary py-3 px-5 border border-primary rounded-md hover:bg-secondary hover:border-secondary duration-200'>Enroll Now</button>
+                {
+                    courses.map(data =>
+                        <div className='border rounded-md'>
+                            <div>
+                                <img src={data.image} className='rounded-t-md' alt="" />
+                            </div>
+                            <div className='p-4'>
+                                <h2 className='text-2xl font-semibold'>{data.title}</h2>
+                                <p className='font-light my-3'>{data.shortDescription}.</p>
+                                <p className='font-semibold'>Course fee {data.price}&#2547;</p>
+                                <div className='mt-4 flex '>
+                                    <button className='mr-4 text-sm text-primary font-semibold border border-primary rounded-md px-5 py-3 hover:bg-primary hover:text-base-100 duration-200'>Overview</button>
+                                    <button className='text-sm text-base-100 font-medium bg-primary py-3 px-5 border border-primary rounded-md hover:bg-secondary hover:border-secondary duration-200'>Enroll Now</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className=''>
-                    <div>
-                        <img src={course1} alt="" />
-                    </div>
-                    <div>
-                        <h2>Graphic Design</h2>
-                        <p>Course fee: 7000$</p>
-                        <div>
-                            <button>Show More</button>
-                            <button>Enroll Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className=''>
-                    <div>
-                        <img src={course1} alt="" />
-                    </div>
-                    <div>
-                        <h2>Graphic Design</h2>
-                        <p>Course fee: 7000$</p>
-                        <div>
-                            <button>Show More</button>
-                            <button>Enroll Now</button>
-                        </div>
-                    </div>
-                </div>
+                    )
+                }
             </div>
         </div>
     );
